2d02187bda2adfc8a027858470a42ef39d08a68c
techscaleups <info.techscaleup@gmail.com>
2025-07-23 18:55:32 +0530

    gym_frontend

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const staging = '/gym-frontend/';
+const staging = '/';
 
 
 const App = () => {
